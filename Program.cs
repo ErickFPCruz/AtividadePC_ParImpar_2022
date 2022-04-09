ca4c45553b509105fc502dd7a5c53cd52bac86a5
ErickFPCruz <100002211+ErickFPCruz@users.noreply.github.com>
2022-04-08 21:59:20 -0300

    Subindo os arquivos em um dia de CÃO!!!!!!!!

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+double numeroEscrito;
+
+Console.Write("Escreva um número qualquer: ");
+numeroEscrito = Convert.ToDouble(Console.ReadLine());
+
+if (numeroEscrito % 2 == 1)
+{
+    Console.WriteLine();
+}
